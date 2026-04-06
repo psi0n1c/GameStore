4fc4d229382c8bb29f9c86fc27b13e232720855c
leo-bozhinoski <leo.bozinoski@gmail.com>
2026-04-06 10:17:50 +0200

    Changed games container from flex to grid and fixed card issue

--- GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+007ca5cd9b4bedde036c67bb02532261d5c9b94b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ecdf3b447a5c1a989772f429d1b0d8c0f7da908")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
