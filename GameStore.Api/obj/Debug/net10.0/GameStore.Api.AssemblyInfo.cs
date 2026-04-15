3cb87f40d34a6786453d07bdffc4324c83a4c786
leo-bozhinoski <leo.bozinoski@gmail.com>
2026-04-15 12:06:27 +0200

    gitignore probably

--- GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6debc9135d4ff036dae181dc0b6234561192e4fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43596827919cb508e7d98fc01b0581aa83159f15")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
